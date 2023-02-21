f4ecd65034f20345060ae2a70f8673f78e1ff725
yuuuba <nsabu45@gmail.com>
2023-02-21 12:01:52 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
-var number = [19, 3, 81, 1, 24, 21];
-console.log(number);
+var sortNumber = function (number) { 
+   number.sort(function (a, b) { 
+       if (a == b) { 
+           return 0; 
+       } 
+       return a < b ? -1 : 1; 
+   }); 
+}; 
+
+変数番号 = [19, 3, 81, 1, 24, 21]; 
+sortNumber(数値); 
+console.log(番号);
